e6645b20ca14e02a11cad01237dda2a0de30c37e
Maud BUCHWALTER <mabuchwa@e2r8p17.42.fr>
2016-02-04 14:52:17 +0100

    GNL

--- get_next_line.c
+++ get_next_line.c
@@ -6,37 +6,66 @@
 /*   By: mabuchwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/01 16:14:40 by mabuchwa          #+#    #+#             */
-/*   Updated: 2016/02/03 11:47:27 by mabuchwa         ###   ########.fr       */
+/*   Updated: 2016/02/04 14:48:58 by mabuchwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int		get_line(char *str, char *tmp, char **line)
+{
+	int				i;
+	int				j;
+
+	i = 0;
+	j = 1;
+	while (tmp[i] != '\0')
+	{
+		if (tmp[i] == '\n')
+		{
+			*line = ft_strsub(tmp, 0, (i));
+//			printf("ligne = %s\n", *line);
+			while (tmp[i + j] != '\0')
+			{
+				str = ft_strsub(tmp, i, (j + 1));
+				j++;
+			}
+			printf("reste = %s\n", str);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
 int		get_next_line(int const fd, char **line)
 {
+	static char		*str;
 	int				ret;
-	int				i;
 	char			buf[BUFF_SIZE + 1];
 	char			*tmp;
 
 	tmp = ft_strnew(1);
-	i = 0;
 	if (fd < 0)
 		return (-1);
 	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[ret] = '\0';
 		tmp = ft_strjoin(tmp, buf);
-		*line = tmp;
-		return (1);
+		if (get_line(str, tmp, line) == 1)
+		{
+			free(tmp);
+			tmp = NULL;
+			return (1);
+		}
 	}
 	return (0);
 }
 
 int		main(int ac, char **av)
 {
-	int			fd;
-	char		**line;
+	int				fd;
+	char			**line;
 
 	line = (char**)malloc(sizeof(*line));
 	fd = open(av[1], O_RDONLY);
@@ -48,7 +77,5 @@ int		main(int ac, char **av)
 		}
 		close(fd);
 	}
-	else
-		write(1, "error\n", 6);
-	return(0);
+	return (0);
 }
